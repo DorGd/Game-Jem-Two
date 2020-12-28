6550050a8bc46eb8c3466a103808cb4dac454dd0
Dori Rosenberg <33037430+Dori203@users.noreply.github.com>
2020-12-28 12:47:16 +0200

    Added raycast water clicking manager, for creating ripples

---
+++ Assets/Scripts/ClickPositionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPositionManager : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask waterLayer;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = -Vector3.one;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 1000f, waterLayer))
+            {
+                clickPosition = hit.point;
+            }
+
+            Debug.Log(clickPosition);
+        }
+        
+
+    }
+}
